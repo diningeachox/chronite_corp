c9e59584374d2ed009eaa814336414bd3eb3d93c
Jack Ding <jackxiaochending@gmail.com>
2023-04-14 00:03:56 -0400

    minor changes

--- assemblage.js
+++ assemblage.js
@@ -3,7 +3,7 @@ ECS.Assemblages = {
     // then have components added or removed - this is just like a helper
     // factory to create objects which can still be modified
 
-    //Training drone
+    //Ship (to create, call ECS.Assemblages.Ship(config) )
     Ship: function Ship(config){
         var mon = new ECS.Entity();
         //var sprite = new Sprite(new Vector2D(31, 25), images["e1"], 3, 4, this.grid_draw_size.scalarMult(2.0));
